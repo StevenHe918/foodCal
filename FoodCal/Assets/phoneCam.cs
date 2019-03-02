b676d42db259e877a516db77b4595701bba5733e
StevenHe918 <zhe5@uci.edu>
2019-03-02 14:55:56 -0800

    camera

---
+++ FoodCal/Assets/phoneCam.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class phoneCam : MonoBehaviour
+{
+    private bool CamAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamTexture[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            CamAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if(!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Scree.height);
+            }
+
+        }
+        if(backCam == null)
+        {
+            Debug.Log("Unable to find back Camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        CamAvailable = true;
+    }
+
+    private void Update()
+    {
+        if(!camAvailable)
+            return;
+        
+        float ratio = (float)backCam.width/(float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f:1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.logcalEulerAngles = new Vector3(0,0, orient);
+    }
+}
+
+
