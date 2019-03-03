69504fdd0e59ffacb0bfd893c264fb07fee7d190
StevenHe918 <zhe5@uci.edu>
2019-03-02 18:47:34 -0800

    update

---
+++ FoodCal/Assets/script/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SceneLoad(int Index)
+    {
+
+        SceneManager.LoadScene(Index);
+
+    }
+
+
+}
---
+++ FoodCal/Assets/script/getSearch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class getSearch : MonoBehaviour
+{
+
+    public InputField Search;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SceneLoader(int Index)
+    {
+
+        SceneManager.LoadScene(Index);
+
+    }
+
+    // Update is called once per frame
+    public void Check_search()
+    {
+        if (true) //the returned value is null or empty string
+            SceneLoader(3);
+    }
+
+}
