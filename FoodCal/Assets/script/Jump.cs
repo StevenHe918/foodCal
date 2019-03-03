bfa0ef664e0bea6c86f5b644d5ab4b108d469ec7
StevenHe918 <zhe5@uci.edu>
2019-03-02 19:09:17 -0800

    event System

--- FoodCal/Assets/script/Jump.cs
+++ FoodCal/Assets/script/Jump.cs
@@ -6,11 +6,7 @@ using UnityEngine.SceneManagement;
 public class Jump : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    // Start is called before the first frame updat
 
     public void SceneLoad(int Index)
     {
